38c288b9b8dfb69064eda0286fd60a3a89fceaab
Christina Sedlmeier <38252323+ChristinaSedlmeier@users.noreply.github.com>
2020-03-10 16:56:44 +0100

    Add files via upload

--- AnswerHighlighter.cs
+++ AnswerHighlighter.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
 public class AnswerHighlighter : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    ToggleGroup toggleGroupInstance;
+
+    public Toggle currentSelection{ 
+        get {
+            return toggleGroupInstance.ActiveToggles().FirstOrDefault();
+        }
+            
+    }    // Start is called before the first frame update
     void Start()
     {
-        
+        toggleGroupInstance = GetComponent<ToggleGroup>();
+        SelectToggle(1);
+    }
+
+    public void SelectToggle(int id)
+    {
+        var toggles = toggleGroupInstance.GetComponentsInChildren<Toggle>();
+        toggles[id].isOn = true;
     }
 
     // Update is called once per frame
